866a6e02090830e62cc2931de52c8f6899efd21f
dinhtanloc <locdinh.31221020226@st.ueh.edu.vn>
2025-06-28 01:00:26 +0700

    Feat: Init repositories for exams

---
+++ lib/repositories/exam_repository.dart
@@ -0,0 +1,15 @@
+import 'package:ueh_mobile_app/models/exam_model.dart';
+import 'package:ueh_mobile_app/database/local_database.dart';
+
+class ExamRepository {
+  final LocalDatabase _dbHelper = LocalDatabase();
+
+  Future<List<ExamModel>> fetchExamsWithFileStatus(List<ExamModel> exams) async {
+    for (var exam in exams) {
+      final hasFile = await _dbHelper.checkIsSaved(exam.examId);
+      exam.isSaved = hasFile;
+    }
+
+    return exams;
+  }
+}
